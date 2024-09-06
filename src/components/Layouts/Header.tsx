267eb159d65f0e211163f9845ccb023fc8c5688c
dvp-tae <th3016@naver.com>
2024-09-06 23:10:31 +0900

    feat: tailwind 초기 재설정 및 헤더 레이아웃 제작

---
+++ src/components/Layouts/Header.tsx
@@ -0,0 +1,16 @@
+export const Header = () => {
+	return (
+		<>
+			<header className="flex justify-between h-[74px] py-6 px-[90px]">
+				<div className="flex justify-center items-center text-[25px]">
+					<div className="w-6 h-6 bg-white"></div>
+					<div>DEVOCEAN</div>
+				</div>
+				<div className="flex text-lg font-medium gap-12">
+					<div>About</div>
+					<div>Project</div>
+				</div>
+			</header>
+		</>
+	);
+};
---
+++ src/pages/Project/page.tsx
@@ -0,0 +1,5 @@
+const Projectpage = () => {
+	return <div></div>;
+};
+
+export default Projectpage;

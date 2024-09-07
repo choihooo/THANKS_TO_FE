ced7724fc21f84090ccc9af7bb9c05d43c0138de
dvp-tae <th3016@naver.com>
2024-09-08 00:37:35 +0900

    feat: 폰트 재설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,27 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {
-			fontFamily: {
-				sktBold: ['SKTSansTTFBold'],
-				sktMedium: ['SKTSansTTFMedium'],
-				sktRegular: ['SKTSansTTFRegular'],
-			},
+		extend: {},
+		fontFamily: {
+			sktBold: ['SKTSansTTFBold'],
+			sktMedium: ['SKTSansTTFMedium'],
+			sktRegular: ['SKTSansTTFRegular'],
+		},
+		fontSize: {
+			lg: [
+				'24px',
+				{
+					lineHeight: '36px',
+					fontWeight: '700',
+				},
+			],
+			sm: [
+				'14px',
+				{
+					lineHeight: '21px',
+					fontWeight: '400',
+				},
+			],
 		},
 	},
 	plugins: [],

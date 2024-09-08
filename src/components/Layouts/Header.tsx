8a2a04e2b646c2f79ff9207b4b2bb8d4b3c9ebc1
dvp-tae <th3016@naver.com>
2024-09-08 15:41:51 +0900

    feat: 헤더 리다렉 기능 추가

--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -4,7 +4,9 @@ import Logo from '../../assets/SVG/DEVOCEAN_LOGO.svg';
 export const Header = () => {
 	return (
 		<header className="fixed top-0 left-0 right-0 flex justify-between items-center h-[74px] py-6 px-[90px] z-10 bg-[#010713]">
-			<img src={Logo} alt="Devocean" className="w-[148px] h-[25px]" />
+			<Link to={'/'}>
+				<img src={Logo} alt="Devocean" className="w-[148px] h-[25px]" />
+			</Link>
 			<nav className="flex font-sktMedium text-sm gap-12">
 				<Link to={'/'}>About</Link>
 				<Link to={'/project'}>Project</Link>

bf7899ad6ed661f62df5db18d371775c5957a354
dvp-tae <th3016@naver.com>
2024-09-08 15:42:48 +0900

    feat: 프로젝트 전체보기, 상세보기 페이지 UI 구현

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import Mainpage from './pages/Main/page';
 import Projectpage from './pages/Project/page';
 import { Layout } from './components/Layouts/Layout';
+import { ProjectAllPage } from './pages/ProjectAll/page';
+import { ProjectDetailPage } from './pages/ProjectDetail/page';
 
 export function Router() {
 	return (
@@ -9,6 +11,8 @@ export function Router() {
 			<Route path="/" element={<Layout />}>
 				<Route path="/" element={<Mainpage />} />
 				<Route path="/project" element={<Projectpage />} />
+				<Route path="/project/all" element={<ProjectAllPage />} />
+				<Route path="/project/:id" element={<ProjectDetailPage />} />
 			</Route>
 		</Routes>
 	);
